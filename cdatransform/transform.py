e8709f026621d00f5b3aedee73b44cbec087c062
Kaushik Ghose <kaushik.ghose@gmail.com>
2021-01-15 06:15:56 -0500

    Simplified version of Transform class.

--- cdatransform/transform.py
+++ cdatransform/transform.py
@@ -1,62 +1,65 @@
 from typing import Union
 import sys
+import logging
 
 import yaml
 from yaml import Loader
 
-from .transformlib import XFunc
-from .gdclib import (entity_to_specimen, demographics, none_to_zero)
 
-v_lib = {
-    "none_to_zero": none_to_zero
-}
+from .gdclib import (base, entity_to_specimen)
 
-s_lib = {
-    "gdc.specimen": entity_to_specimen
-    , "gdc.demographics": demographics
+logger = logging.getLogger(__name__)
+
+
+t_lib = {
+    "gdc.research_subject": base,
+    "gdc.entity_to_specimen": entity_to_specimen
 }
 
 
 def parse_transforms(t_list, t_lib):
-    sys.stderr.write(f"Transforms that will be applied:\n")
+    logger.info(f"Loading transforms")
+
     _transforms = []
-    for xform in t_list:
-        f = parse_transform_call(xform, t_lib)
-        if f is not None:
-            _transforms += [f]
-    return _transforms
-
-
-def parse_transform_call(xform, t_lib):
-    if isinstance(xform, dict):
-        t_name, t_params = next(iter(xform.items()))
-    else:
-        t_name = xform
-        t_params = {}
+    if not isinstance(t_list, list):
+        logger.error("Transforms must be a list")
+        return _transforms
     
-    f = t_lib.get(t_name)
-    if f is not None:
-        sys.stdout.write(f"{t_name}: {f.__doc__}\n")
-        return XFunc(func=f, params=t_params)
-    else:
-        sys.stdout.write(f"{t_name}: Unknown transform!\n")
-        return None
+    for n, xform in enumerate(t_list):
+        if isinstance(xform, str):
+            _f, _p = xform, {}
+        elif isinstance(xform, dict):
+            if len(xform.keys()) != 1:
+                logger.error(f"Transform #{n} is ill-formed")
+                continue
+            for _k, _v in xform.items():
+                _f, _p = _k, _v
+        else:
+            logger.error(f"Transform #{n} is ill-formed")
+            continue
+
+        if _f not in t_lib:
+            logger.error(f"unknown transform '{_f}'")
+            continue
+
+        _func = t_lib[_f]
+        logger.info(f"Added transform {_f}: {_func.__doc__}")
+        _transforms += [(_func, _p)]
+
+    if len(_transforms) != len(t_list):
+        _transforms = []
+        logger.error("Will not run while there are issues with transforms.")
+
+    return _transforms
 
 
 class Transform:
     def __init__(self, transform_file) -> None:
         t_list = yaml.load(open(transform_file, "r"), Loader=Loader)
-        self._v_transforms = parse_transforms(t_list.get("value"), v_lib)
-        self._s_transforms = parse_transforms(t_list.get("structural"), s_lib)
+        self._transforms = parse_transforms(t_list, t_lib)
 
-    def __call__(self, case: dict) -> dict:
-        # Parameter passing?
-        # Transform error logging?
-        v_case = case
-        for vt in self._v_transforms:
-            v_case = vt.func(v_case, **vt.params)
-
-        t_case = {}
-        for st in self._s_transforms:
-            t_case = st.func(t_case, v_case, **st.params)
-        return t_case
+    def __call__(self, source: dict) -> dict:
+        destination = {}
+        for vt in self._transforms:
+            destination = vt[0](destination, source, **vt[1])
+        return destination
