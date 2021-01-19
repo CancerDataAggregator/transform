a6b80b47040bdabb086e9df9479485631dd7d45d
Kaushik Ghose <kaushik.ghose@gmail.com>
2021-01-19 10:39:03 -0500

    Refactor: Improve code for filtering cases

--- cdatransform/transform/main.py
+++ cdatransform/transform/main.py
@@ -10,64 +10,28 @@ from typing import Union
 import yaml
 from yaml import Loader
 
-from cdatransform.extract.lib import get_case_ids
-from .gdclib import (research_subject, diagnosis, entity_to_specimen)
+from cdatransform.lib import get_case_ids
+from cdatransform.transform.lib import Transform
+
 
 logger = logging.getLogger(__name__)
 
-t_lib = {
-    "gdc.research_subject": research_subject,
-    "gdc.diagnosis": diagnosis,
-    "gdc.entity_to_specimen": entity_to_specimen
-}
 
-
-def parse_transforms(t_list, t_lib):
-    logger.info(f"Loading transforms")
-
-    _transforms = []
-    if not isinstance(t_list, list):
-        logger.error("Transforms must be a list")
-        return _transforms
+def filter_cases(reader, case_list):
+    if case_list is None:
+        cases = None
+    else:
+        cases = set(case_list)
     
-    for n, xform in enumerate(t_list):
-        if isinstance(xform, str):
-            _f, _p = xform, {}
-        elif isinstance(xform, dict):
-            if len(xform.keys()) != 1:
-                logger.error(f"Transform #{n} is ill-formed")
-                continue
-            for _k, _v in xform.items():
-                _f, _p = _k, _v
+    for case in reader:
+        if cases is None:
+            yield case
+        elif len(cases) == 0:
+            break
         else:
-            logger.error(f"Transform #{n} is ill-formed")
-            continue
-
-        if _f not in t_lib:
-            logger.error(f"unknown transform '{_f}'")
-            continue
-
-        _func = t_lib[_f]
-        logger.info(f"Added transform {_f}: {_func.__doc__}")
-        _transforms += [(_func, _p)]
-
-    if len(_transforms) != len(t_list):
-        _transforms = []
-        logger.error("Will not run while there are issues with transforms.")
-
-    return _transforms
-
-
-class Transform:
-    def __init__(self, transform_file) -> None:
-        t_list = yaml.load(open(transform_file, "r"), Loader=Loader)
-        self._transforms = parse_transforms(t_list, t_lib)
-
-    def __call__(self, source: dict) -> dict:
-        destination = {}
-        for vt in self._transforms:
-            destination = vt[0](destination, source, **vt[1])
-        return destination
+            if case.get("id") in cases:
+                cases.pop(case.get("id"))
+                yield case
 
 
 def main():
@@ -97,24 +61,13 @@ def main():
 
     t0 = time.time()
     count = 0
-    case_list = None
-    if case is not None:
-        case_list = set(case)
-    elif case_f is not None:
-        case_list = get_case_ids(case_list_file=case_f)
+    case_list = get_case_ids(case=args.case, case_list_file=case_f)
 
     with gzip.open(in_file, "r") as infp:
         with gzip.open(out_file, "w") as outfp:
             reader = jsonlines.Reader(infp)
             writer = jsonlines.Writer(outfp)
-            for case in reader:
-                if case_list is not None:
-                    if len(case_list) == 0:
-                        break
-                    if case.get("id") not in case_list:
-                        continue
-                    case_list.pop(case.get("id"))
-
+            for case in filter_cases(reader, case_list=case_list):
                 writer.write(transform(case))
                 count += 1                
                 if count % 5000 == 0:
