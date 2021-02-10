9dcf2e2f11ce158ffdf157dbabfc7abc8c11ed1b
Veso <ana.damljanovic@sbgenomics.com>
2021-02-10 11:49:06 +0100

    Add GDC files and caching

--- cdatransform/extract/gdc.py
+++ cdatransform/extract/gdc.py
@@ -1,16 +1,17 @@
-import requests
 import json
 import jsonlines
 import gzip
 import sys
 import time
 import argparse
+from collections import defaultdict
+import os
 
 from cdatransform.lib import get_case_ids
 from cdatransform.extract.lib import retry_get
 
 
-default_fields = [
+cases_fields = [
     "case_id",
     "submitter_id",
     "disease_type",
@@ -32,10 +33,6 @@ default_fields = [
     "samples.submitter_id",
     "samples.sample_type",
     "samples.biospecimen_anatomic_site",
-    "samples.portions.portion_id",
-    "samples.portions.slide_id",
-    "samples.portions.analytes.analyte_id",
-    "samples.portions.analytes.aliquots.aliquot_id",
     "files.file_id",
     "files.file_name",
     "files.file_size",
@@ -43,6 +40,12 @@ default_fields = [
     "files.revision",
     "files.tags",
     "files.type",
+    "files.data_type"
+]
+
+files_fields = [
+    "file_id",
+    "cases.samples.sample_id"
 ]
 
 
@@ -52,19 +55,36 @@ def clean_fields(hit):
     return hit
 
 
+def get_total_number(endpoint):
+    params = {
+        'format': 'json'
+    }
+    result = retry_get(endpoint, params=params)
+    return result.json()['data']['pagination']['total']
+
+
 class GDC:
     def __init__(
-        self, endpoint="https://api.gdc.cancer.gov/v0/cases", fields=default_fields
+        self,
+        cases_endpoint="https://api.gdc.cancer.gov/v0/cases",
+        files_endpoint="https://api.gdc.cancer.gov/v0/files",
+        cases_fields=cases_fields,
+        files_fields=files_fields,
+        cached_files="pdc_files_cached.json.gz"
     ) -> None:
-        self.endpoint = endpoint
-        self.fields = fields
+
+        self.cases_endpoint = cases_endpoint
+        self.files_endpoint = files_endpoint
+        self.cases_fields = cases_fields
+        self.files_fields = files_fields
+        self.cached_files = cached_files
 
     def cases(
         self,
         case_ids=None,
         page_size=100,
     ):
-        fields = ",".join(self.fields)
+        fields = ",".join(self.cases_fields)
         # defining the GDC API query
         if case_ids is not None:
             filt = json.dumps(
@@ -78,9 +98,10 @@ class GDC:
         else:
             filt = None
 
-        offset = 0
+        total_cases = get_total_number(self.cases_endpoint)
         t0 = time.time()
-        while True:
+
+        for offset in range(0, total_cases, page_size):
             params = {
                 "filters": filt,
                 "format": "json",
@@ -89,30 +110,110 @@ class GDC:
                 "from": offset,
             }
 
-            # How to handle errors
-            result = retry_get(self.endpoint, params=params)
+            sys.stderr.write(f"Pulling cases page {int(offset / page_size) + 1}/{int(total_cases / page_size) + 1}\n")
+            result = retry_get(self.cases_endpoint, params=params)
             hits = result.json()["data"]["hits"]
-            page = result.json()["data"]["pagination"]
-            p_no = page.get("page")
-            p_tot = page.get("pages")
-
-            sys.stderr.write(f"Pulling page {p_no} / {p_tot}\n")
 
             for hit in hits:
                 yield clean_fields(hit)
 
-            if p_no >= p_tot:
-                break
+    def files(
+            self,
+            cached=False,
+            page_size=10000
+    ):
+        if cached:
+            if not os.path.isfile(self.cached_files):
+                sys.stderr.write("Cached file not found. Pulling from the API.\n")
+                pull_from_api = True
             else:
-                offset += page_size
+                pull_from_api = False
+        else:
+            pull_from_api = True
 
-    def save_cases(self, out_file, case_ids=None, page_size=1000):
+        if pull_from_api:
+            fields = ",".join(self.files_fields)
+            total_files = get_total_number(self.files_endpoint)
+
+            with gzip.open(self.cached_files, "wb") as cache_file:
+                writer = jsonlines.Writer(cache_file)
+                for offset in range(0, total_files, page_size):
+                    params = {
+                        'format': 'json',
+                        'fields': fields,
+                        'sort': 'file_id',
+                        'from': offset,
+                        'size': 10000
+                    }
+                    sys.stderr.write(f"Pulling files page {int(offset / page_size) + 1}/{int(total_files / page_size) + 1}\n")
+                    result = retry_get(self.files_endpoint, params=params)
+                    hits = result.json()["data"]["hits"]
+
+                    for hit in hits:
+                        writer.write(hit)
+                        yield hit
+        else:
+            if os.path.isfile(self.cached_files):
+                sys.stderr.write("Using cached files.\n")
+                with gzip.open(self.cached_files, "rb") as cache_file:
+                    for hit in cache_file:
+                        yield json.loads(hit)
+
+    def get_files_per_sample(self, cached=False) -> dict:
+        files_per_sample = defaultdict(list)
+        sys.stderr.write(f"Started collecting files.\n")
+
+        t0 = time.time()
+
+        for hit in self.files(cached=cached):
+            file_id = hit.get("file_id")
+            cases = hit.get("cases")
+            if cases is not None:
+                for case in cases:
+                    samples = case["samples"]
+                    if samples is not None:
+                        for sample in case["samples"]:
+                            sample_id = sample.get("sample_id")
+                            files_per_sample[sample_id].append(file_id)
+
+        sys.stderr.write(f"Created a files look-up dict for {len(files_per_sample)} samples in {time.time() - t0}s\n")
+        return files_per_sample
+
+    def rearrange_fields(self, case_record, files_per_sample) -> dict:
+        new_case_record = dict()
+        new_case_fields = [
+            field.split(".")[0]
+            for field in self.cases_fields
+            if not field.startswith("files.")
+        ]
+        for new_field in new_case_fields:
+            if case_record.get(new_field) is not None:
+                new_case_record[new_field] = case_record.get(new_field)
+
+        samples = new_case_record.get("samples")
+        # By following condition, adding files for
+        # cases that don't have samples is skipped
+        if samples:
+            for index, sample in enumerate(new_case_record.get("samples")):
+                new_case_record["samples"][index]["files"] = []
+                sample_id = sample.get("sample_id")
+                file_ids = files_per_sample.get(sample_id)
+                case_files = case_record.get("files")
+                if file_ids and case_files:
+                    for f in case_files:
+                        if f.get("file_id") in set(file_ids):
+                            new_case_record["samples"][index]["files"].append(f)
+        return new_case_record
+
+    def save_cases(self, out_file, case_ids=None, cached=False, page_size=1000):
         t0 = time.time()
         n = 0
+        files_per_sample = self.get_files_per_sample(cached)
         with gzip.open(out_file, "wb") as fp:
             writer = jsonlines.Writer(fp)
             for case in self.cases(case_ids, page_size):
-                writer.write(case)
+                case_with_specimen_files = self.rearrange_fields(case, files_per_sample)
+                writer.write(case_with_specimen_files)
                 n += 1
                 if n % page_size == 0:
                     sys.stderr.write(f"Wrote {n} cases in {time.time() - t0}s\n")
@@ -126,11 +227,12 @@ def main():
     parser.add_argument(
         "--cases", help="Optional file with list of case ids (one to a line)"
     )
+    parser.add_argument("--cache", help="Use cached files.", action='store_true')
     args = parser.parse_args()
 
     gdc = GDC()
     gdc.save_cases(
-        args.out_file, case_ids=get_case_ids(case=args.case, case_list_file=args.cases)
+        args.out_file, case_ids=get_case_ids(case=args.case, case_list_file=args.cases), cached=args.cache
     )
 
 
