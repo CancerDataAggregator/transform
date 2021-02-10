35813f2b0f6dba2ebbcce3459523752a608897d0
Ana <ana.damljanovic@sbgenomics.com>
2021-02-10 19:31:25 +0100

    PDC: attach file information to samples (#16)
    1. Pull file metadata from PDC API
    2. Employ cache for file information
    3. Use git-lfs to store file
    4. Update integration tests
    
    Co-authored-by: Phil Shapiro <pshapiro@broadinstitute.org>
    Co-authored-by: Kaushik Ghose <kaushik.ghose@gmail.com>

---
+++ cdatransform/extract/pdc_query_lib.py
@@ -0,0 +1,72 @@
+from string import Template
+
+
+def query_all_cases():
+    query = """{
+  allCases {
+    case_id
+  }
+}"""
+    return query
+
+
+def query_single_case(case_id):
+    template = Template(
+        """{
+  case(case_id: "$case_id") {
+    case_id
+    case_submitter_id
+    disease_type
+    primary_site
+    project_submitter_id
+    demographics {
+      ethnicity
+      gender
+      race
+      days_to_birth
+    }
+    samples {
+      sample_id
+      sample_submitter_id
+      biospecimen_anatomic_site
+      sample_type
+    }
+    diagnoses {
+      age_at_diagnosis
+      diagnosis_id
+      tumor_grade
+      tumor_stage
+      morphology
+      primary_diagnosis
+    }
+  }
+}
+"""
+    )
+
+    query = template.substitute(case_id=case_id)
+    return query
+
+
+def query_files_bulk(offset, limit):
+    template = Template(
+        """{
+  fileMetadata(offset: $offset, limit: $limit) {
+    file_id
+    file_name
+    file_location
+    md5sum
+    file_size
+    file_submitter_id
+    data_category
+    file_type
+    file_format
+    experiment_type
+    aliquots {
+      sample_id
+    }
+  }
+}"""
+    )
+    query = template.substitute(offset=offset, limit=limit)
+    return query
