f4819dbc128eb8436028cb1aaa19fe865d587e13
Phil Shapiro <pshapiro@broadinstitute.org>
2021-02-17 10:44:20 -0500

    Add support for passing a list of tables to the validation logging functions (#40)
    With this change, the table argument can either be a single table or a list of tables.

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -11,12 +11,8 @@ class TestLogValidation(TestCase):
         t1 = {"field": "val1", "field2": "val3"}
         t2 = {"field": "val2"}
         t3 = {"field2": "val3"}
-        log.distinct(t1, "field")
-        log.distinct(t2, "field")
-        log.distinct(t3, "field")
-        log.distinct(t1, "field2")
-        log.distinct(t2, "field2")
-        log.distinct(t3, "field2")
+        log.distinct([t1, t2, t3], "field")
+        log.distinct([t1, t2, t3], "field2")
 
         with self.assertLogs('test', level='INFO') as cm:
             log.generate_report(logging.getLogger('test'))
@@ -29,9 +25,8 @@ class TestLogValidation(TestCase):
         t1 = {"field1": "val1", "field2": "val3"}
         t2 = {"field1": "val2", "field2": "val4"}
         t3 = {"field1": "val1", "field2": "val5"}
-        log.agree(t1, "id1", ["field1", "field2"])
+        log.agree([t1, t3], "id1", ["field1", "field2"])
         log.agree(t2, "id2", ["field1", "field2"])
-        log.agree(t3, "id1", ["field1", "field2"])
 
         with self.assertLogs('test', level='INFO') as cm:
             log.generate_report(logging.getLogger('test'))
@@ -46,8 +41,7 @@ class TestLogValidation(TestCase):
         def is_even(x):
             return x % 2 == 0
 
-        log.validate(t1, "field1", is_even)
-        log.validate(t2, "field1", is_even)
+        log.validate([t1, t2], "field1", is_even)
 
         with self.assertLogs('test', level='INFO') as cm:
             log.generate_report(logging.getLogger('test'))
