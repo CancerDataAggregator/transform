97ef1c0a86f552d158c48c19f3ac0ec478e097c4
Kaushik Ghose <kaushik.ghose@gmail.com>
2021-02-23 14:36:41 -0500

    Clean up upload script.

--- data/upload.sh
+++ data/upload.sh
@@ -1,7 +1,5 @@
 set -ex
 
-# Generate GDC examples
-
 extract-gdc gdc.jsonl.gz gdc.samples-per-file.jsonl.gz gdc.fileuuid.jsonl.gz
 cda-transform gdc.jsonl.gz gdc.H.jsonl.gz ../gdc-transform.yml
 cda-aggregate ../merge.yml gdc.H.jsonl.gz gdc.A.jsonl.gz
@@ -12,4 +10,4 @@ cda-aggregate ../merge.yml pdc.H.jsonl.gz pdc.A.jsonl.gz
 
 cda-merge ../merge.yml gdc.pdc.M.jsonl.gz --gdc gdc.A.jsonl.gz --pdc pdc.A.jsonl.gz
 
-# bq load --autodetect --source_format NEWLINE_DELIMITED_JSON cda_mvp.v1 gdc.pdc.M.jsonl.gz
+# bq load --autodetect --source_format NEWLINE_DELIMITED_JSON cda_mvp.v2 gdc.pdc.M.jsonl.gz
