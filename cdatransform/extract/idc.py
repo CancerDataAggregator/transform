e58a4b937044bfd50eb8f655c598107925cd7e1d
druth314 <druth314@gmail.com>
2021-08-31 15:02:34 -0400

    Version 3 of IDC ETL code - Run query that transforms data directly from IDC to CDA table

--- cdatransform/extract/idc.py
+++ cdatransform/extract/idc.py
@@ -1,3 +1,15 @@
+import json
+import jsonlines
+import gzip
+import sys
+import time
+from collections import defaultdict
+import pathlib
+import yaml
+from yaml import Loader
+
+from cdatransform.lib import get_case_ids
+from cdatransform.extract.lib import retry_get
 import argparse
 from google.cloud import bigquery, storage
 from google.oauth2 import service_account
@@ -23,20 +35,25 @@ class IDC:
     def __init__(
         self,
         gsa_key='../../GCS-service-account-key.json',
-        cases_file=None,
-        case=None,
-        dest_table_id='gdc-bq-sample.idc_test.dicom_pivot_wave1',
-        dest_bucket='gdc-bq-sample-bucket',
-        dest_bucket_file_name='druth/idc-extract.jsonl.gz',
-        out_file='idc-test.jsonl.gz'
+        patients_file=None,
+        patient=None,
+        dest_table_id='gdc-bq-sample.idc_test.dicom_pivot_v3',
+        mapping=yaml.load(open('IDC_mapping.yml', "r"), Loader=Loader),
+        source_table='canceridc-data.idc_v3.dicom_pivot_v3',
+        # dest_bucket='gdc-bq-sample-bucket',
+        # dest_bucket_file_name='druth/idc-extract.jsonl.gz',
+        # out_file='idc-test.jsonl.gz'
     ) -> None:
         self.gsa_key = gsa_key
         self.dest_table_id = dest_table_id
-        self.dest_bucket = dest_bucket
-        self.out_file = out_file
-        self.dest_bucket_file_name = dest_bucket_file_name
+        # self.dest_bucket = dest_bucket
+        # self.out_file = out_file
+        # self.dest_bucket_file_name = dest_bucket_file_name
         self.service_account_cred = self._service_account_cred()
-        self.case_ids = get_case_ids(case=case, case_list_file=cases_file)
+        self.mapping = mapping
+        self.patient_ids = get_case_ids(case=patient, case_list_file=patients_file)
+        self.source_table = source_table
+        self.transform_query = self._query_build()
 
     def _service_account_cred(self):
         key_path = self.gsa_key
@@ -56,14 +73,15 @@ class IDC:
         job_config = bigquery.QueryJobConfig(
             allow_large_results=True, destination=dest_table_id,
             write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE)
-        sql = ' '.join(["""
-        SELECT""", """, """.join(idc_fields), """
-        FROM canceridc-data.idc_views.dicom_pivot_wave1
-        """])
-        if self.case_ids is not None:
-            sql = ' '.join([sql, "WHERE crdc_instance_uuid in ('"])
-            cases_str = "','".join(self.case_ids)
-            sql = sql + cases_str + "')"
+        sql = self.transform_query
+        # sql = ' '.join(["""
+        # SELECT""", """, """.join(idc_fields), """
+        # FROM canceridc-data.idc_views.dicom_pivot_wave1
+        # """])
+        # if self.case_ids is not None:
+        #    sql = ' '.join([sql, "WHERE crdc_instance_uuid in ('"])
+        #    cases_str = "','".join(self.case_ids)
+        #    sql = sql + cases_str + "')"
 
         # Start the query, passing in the extra configuration.
         query_job = client.query(sql, job_config=job_config)  # Make an API request.
@@ -114,43 +132,102 @@ class IDC:
             )
         )
 
+    def field_line(self, k, val):
+        if isinstance(val,str):
+            val_split = val.split('.')
+            if len(val_split) > 1:
+                val_split.pop(0)
+                val_split = val_split[0]
+            elif val_split[0] == 'NULL':
+                val_split = 'STRING(NULL)'
+            else:
+                val_split = "'" + val_split[0] + "'"
+            return (val_split) + """ AS """ + k
+        elif isinstance(val,dict):
+            temp = "[STRUCT("
+            keys = list(val.keys())
+            for index in range(len(keys)):
+                temp += self.field_line(keys[index],val[keys[index]])
+                if index < len(keys)-1:
+                    temp += ', '
+            temp +=")] AS " + k
+            return temp
+        else:
+            return """Null""" + """ AS """ + k
+        return val
+    
+    def add_entity_fields(self, entity):
+        entity_string = ''
+        keys = list(self.mapping[entity]['Mapping'].keys())
+        for index in range(len(self.mapping[entity]['Mapping'].keys())):
+            entity_string += self.field_line(keys[index], self.mapping[entity]['Mapping'][keys[index]])
+            if index < len(keys)-1:
+                entity_string += """, """
+        return entity_string
+    
+    def build_where_patients(self):
+        where = ''
+        if self.patient_ids is not None:
+            where = """WHERE PatientID in ("""
+            where += """','""".join(self.patient_ids)+"""')"""
+        return where
+    
+    def _query_build(self, **kwargs):
+        query = """SELECT """
+        query += self.add_entity_fields('Patient')
+        query += """, """
+        # add File record structure to query
+        query += """ARRAY_AGG(STRUCT("""
+        query += self.add_entity_fields('File')
+        query += """)) as File """
+        # add WHERE statement if just looking for specific patients
+        query += """FROM `""" + self.source_table + """`"""
+        query += self.build_where_patients()
+        query += """ GROUP by id"""
+        return query
 
 def main():
     parser = argparse.ArgumentParser(description="Pull case data from GDC API.")
-    parser.add_argument("out_file", help="Out file name. Should end with .gz",
-                        default='idc_extract.jsonl.gz')
+    parser.add_argument("mapping_file", help="Location of IDC mapping file")
+    parser.add_argument("--dest_table_id", help="Permanent table destination after querying IDC",
+                        default='gdc-bq-sample.idc_test.dicom_pivot_v3')
+    parser.add_argument("--source_table", help="IDC source table to be queried",
+                        default='canceridc-data.idc_v3.dicom_pivot_v3')
     parser.add_argument("--gsa_key", help="Location of user GSA key")
-    parser.add_argument("--case", help="Extract just this case", default=None)
+    parser.add_argument("--patient", help="Extract just this patient", default=None)
     parser.add_argument(
-        "--cases", help="Optional file with list of case ids (one to a line)", default=None)
+        "--patients", help="Optional file with list of patient ids (one to a line)", default=None)
+    parser.add_argument("--out_file", help="Out file name. Should end with .gz",
+                        default='idc_extract.jsonl.gz')
     parser.add_argument("--cache", help="Use cached files.", action="store_true")
     parser.add_argument("--make_bq_table", help="Create new BQ permanent table from IDC view",
                         default=False, type=bool)
     parser.add_argument("--make_bucket_file", help="Create new file in GCS from permanent table",
                         default=False, type=bool)
-    parser.add_argument("--dest_table_id", help="Permanent table destination after querying IDC",
-                        default='gdc-bq-sample.idc_test.dicom_pivot_wave1')
     parser.add_argument("--dest_bucket", help="GCS bucket", default='gdc-bq-sample-bucket')
     parser.add_argument("--dest_bucket_file_name", help="GCS bucket file name",
                         default='idc-test.jsonl.gz')
     args = parser.parse_args()
     make_bq_table = args.make_bq_table
     make_bucket_file = args.make_bucket_file
+    mapping = yaml.load(open(args.mapping_file, "r"), Loader=Loader)
     out_file = args.out_file
     idc = IDC(
         gsa_key=args.gsa_key,
         dest_table_id=args.dest_table_id,
-        cases_file=args.cases,
-        case=args.case,
-        dest_bucket=args.dest_bucket,
-        dest_bucket_file_name=args.dest_bucket_file_name,
-        out_file=out_file,
+        patients_file=args.patients,
+        patient=args.patient,
+        mapping=mapping,
+        source_table=args.source_table
+        # dest_bucket=args.dest_bucket,
+        # dest_bucket_file_name=args.dest_bucket_file_name,
+        # out_file=out_file,
     )
     if make_bq_table:
         idc.query_idc_to_table(idc_fields)
-    if make_bucket_file:
-        idc.table_to_bucket()
-    idc.download_blob()
+    # if make_bucket_file:
+    #    idc.table_to_bucket()
+    # idc.download_blob()
 
 
 if __name__ == "__main__":
