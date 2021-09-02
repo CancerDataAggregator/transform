bee3c93cbef48919efc29b61575d4856f5da1042
druth314 <druth314@gmail.com>
2021-09-02 13:46:22 -0400

    adjust files name in pdc

--- tests/integration/check-pdc-pull.py
+++ tests/integration/check-pdc-pull.py
@@ -8,11 +8,12 @@ from cdatransform.lib import get_case_ids
 
 
 def check_case_00dc49de(d):
+    #print(d)
     assert d.get("case_id") == "00dc49de-1fba-11e9-b7f8-0a80fada099c"
     assert d.get("case_submitter_id") == "NCI7-2"
     assert len(d.get("demographics")) == 1
     assert len(d.get("samples")) == 1
-    assert d.get("samples")[0].get("File")[0].get("file_id") is not None
+    assert d.get("samples")[0].get("files")[0].get("file_id") is not None
 
 
 def main():
@@ -26,6 +27,7 @@ def main():
     with gzip.open(args.pull_file, "rb") as f_in:
         reader = jsonlines.Reader(f_in)
         for d in reader:
+            #print(d)
             assert d.get("case_id") in case_ids
             if d.get("case_id") == "00dc49de-1fba-11e9-b7f8-0a80fada099c":
                 check_case_00dc49de(d)
