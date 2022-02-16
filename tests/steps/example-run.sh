8a2da60282d3a6d98d3af13719486e809410ff1d
druth314 <druth314@gmail.com>
2022-02-16 10:12:25 -0500

    edit example run

--- tests/steps/example-run.sh
+++ tests/steps/example-run.sh
@@ -4,22 +4,39 @@ set -ex
 
 extract-gdc gdc_TARGET_case1.jsonl.gz ../../data/gdc.files-specimens-cases.jsonl.gz --case 7eeced68-1717-4116-bcee-328ac70a9682
 extract-gdc gdc_TARGET_case2.jsonl.gz ../../data/gdc.files-specimens-cases.jsonl.gz --case 9e229e56-f7e1-58f9-984b-a9453be5dc9a
+extract-gdc gdc_TARGET_file1.jsonl.gz ../../data/gdc.files-specimens-cases.jsonl.gz --file 6892c6ba-66aa-4083-a13e-845ae272700a
+extract-gdc gdc_TARGET_file2.jsonl.gz ../../data/gdc.files-specimens-cases.jsonl.gz --file eb66f012-158d-409c-b79b-c4cc605eebb3
 
 gunzip -c gdc_TARGET_case1.jsonl.gz > gdc_TARGET_case1.json
 gunzip -c gdc_TARGET_case2.jsonl.gz > gdc_TARGET_case2.json
+gunzip -c gdc_TARGET_file1.jsonl.gz > gdc_TARGET_file1.json
+gunzip -c gdc_TARGET_file2.jsonl.gz > gdc_TARGET_file2.json
 
-cda-transform gdc_TARGET_case1.jsonl.gz gdc.case1.H.json.gz ../../GDC_mapping.yml GDC
+cda-transform gdc_TARGET_case1.jsonl.gz gdc.case1.H.json.gz ../../GDC_mapping.yml GDC --endpoint cases
 python json2yaml.py gdc.case1.H.json.gz gdc.case1.H.yml
 
-cda-transform gdc_TARGET_case2.jsonl.gz gdc.case2.H.json.gz ../../GDC_mapping.yml GDC
+cda-transform gdc_TARGET_case2.jsonl.gz gdc.case2.H.json.gz ../../GDC_mapping.yml GDC --endpoint cases
 python json2yaml.py gdc.case2.H.json.gz gdc.case2.H.yml
 
+cda-transform gdc_TARGET_file1.jsonl.gz gdc.file1.H.json.gz ../../GDC_mapping.yml GDC --endpoint files
+python json2yaml.py gdc.file1.H.json.gz gdc.file1.H.yml
 
-cat gdc_TARGET_case1.jsonl.gz gdc_TARGET_case2.jsonl.gz > gdc.jsonl.gz
+cda-transform gdc_TARGET_file2.jsonl.gz gdc.file2.H.json.gz ../../GDC_mapping.yml GDC --endpoint files
+python json2yaml.py gdc.file2.H.json.gz gdc.file2.H.yml
 
-cda-transform gdc.jsonl.gz gdc.H.jsonl.gz ../../GDC_mapping.yml GDC
-cda-aggregate ../../merge.yml gdc.H.jsonl.gz gdc.A.jsonl.gz
-python json2yaml.py gdc.A.jsonl.gz gdc.A.yml
+cat gdc_TARGET_case1.jsonl.gz gdc_TARGET_case2.jsonl.gz > gdc.cases.jsonl.gz
+cat gdc_TARGET_file1.jsonl.gz gdc_TARGET_file2.jsonl.gz > gdc.files.jsonl.gz
+
+# Aggregation is necessary from the cases endpoint. The top level Subject entity may have more than
+# one ResearchSubject/case associated with it. Aggregate ResearchSubjects/cases of the same Subject
+cda-transform gdc.cases.jsonl.gz gdc.cases.H.jsonl.gz ../../GDC_mapping.yml GDC --endpoint cases
+cda-aggregate ../../merge.yml gdc.cases.H.jsonl.gz gdc.cases.A.jsonl.gz
+python json2yaml.py gdc.cases.A.jsonl.gz gdc.cases.A.yml
+
+cda-transform gdc.files.jsonl.gz gdc.files.H.jsonl.gz ../../GDC_mapping.yml GDC --endpoint files
+# Aggregation of files transformations for GDC is unnecessary. File is top level entity and extracted that 
+# way from the GDC API. Hooray!
+python json2yaml.py gdc.files.H.jsonl.gz gdc.files.H.yml
 
 
 # Generate PDC examples
