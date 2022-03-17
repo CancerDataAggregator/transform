d4a5be861cb4da11b2bf2e6f0a62c879c0b9f3b8
druth314 <druth314@gmail.com>
2022-03-16 23:42:53 -0400

    add google libraries to required list

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 current_path = pathlib.Path(__file__).parent
 
 name = "cdatransform"
-version = open("cdatransform/version.py").read().split("=")[1].strip().strip("\"")
+version = open("cdatransform/version.py").read().split("=")[1].strip().strip('"')
 now = datetime.utcnow()
 desc_path = pathlib.Path(current_path, "README.md")
 long_description = desc_path.open("r").read()
@@ -14,26 +14,27 @@ setup(
     name=name,
     version=version,
     packages=find_packages(),
-    platforms=['POSIX', 'MacOS', 'Windows'],
-    python_requires='>=3.6',
+    platforms=["POSIX", "MacOS", "Windows"],
+    python_requires=">=3.6",
     install_requires=[
         "requests",
         "jsonlines",
         "pyyaml",
-        "deepdiff"
+        "deepdiff",
+        "google-cloud-storage",
+        "google-cloud-bigquery",
     ],
     entry_points={
-        'console_scripts': [
-            'extract-gdc = cdatransform.extract.gdc:main',
-            'extract-pdc = cdatransform.extract.pdc:main',
-            'extract-idc = cdatransform.extract.idc:main',
-            'cda-transform = cdatransform.transform.main:main',
-            'cda-aggregate = cdatransform.transform.aggregate:main',
-            'cda-merge = cdatransform.transform.mergecli:main'
+        "console_scripts": [
+            "extract-gdc = cdatransform.extract.gdc:main",
+            "extract-pdc = cdatransform.extract.pdc:main",
+            "extract-idc = cdatransform.extract.idc:main",
+            "cda-transform = cdatransform.transform.main:main",
+            "cda-aggregate = cdatransform.transform.aggregate:main",
+            "cda-merge = cdatransform.transform.mergecli:main",
         ],
     },
-
-    description='Performs transforms to hamonize DC data to CDA',
+    description="Performs transforms to hamonize DC data to CDA",
     long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
