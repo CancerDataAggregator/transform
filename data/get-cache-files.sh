fe4d864db204b8997c6601816e94f1ced043407e
druth314 <druth314@gmail.com>
2022-03-17 07:55:28 -0400

    get cache files with gsutil, test auto auth

--- data/get-cache-files.sh
+++ data/get-cache-files.sh
@@ -3,6 +3,7 @@ set -ex
 # If the cache files are missing, download them.
 for FILE in gdc.files-specimens-cases.jsonl.gz pdc.files-specimens-cases.jsonl.gz pdc.all_cases.jsonl.gz all_subjects.jsonl.gz; do
     if [ ! -f "$FILE" ]; then
-      curl --remote-name https://storage.googleapis.com/broad-cda-dev/public/$FILE
+      gsutil cp gs://broad-cda-dev/public/$FILE
+      #curl --remote-name https://storage.googleapis.com/broad-cda-dev/public/$FILE
     fi
 done
