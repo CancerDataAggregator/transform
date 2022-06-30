a9955bbd683d61a95936cf81e3c300210b9fac65
druth314 <druth314@gmail.com>
2022-06-30 15:42:22 -0400

    update some integration tests

--- data/get-cache-files.sh
+++ data/get-cache-files.sh
@@ -1,9 +1,9 @@
 set -ex
 
 # If the cache files are missing, download them.
-for FILE in gdc.files-specimens-cases.jsonl.gz pdc.files-specimens-cases.jsonl.gz pdc.all_cases.jsonl.gz all_subjects.jsonl.gz; do
+for FILE in gdc.files-specimens-cases.jsonl.gz pdc.files-specimens-cases.jsonl.gz pdc.all_cases.jsonl.gz all_Subjects.jsonl.gz; do
     if [ ! -f "$FILE" ]; then
-      gsutil cp gs://broad-cda-dev/public/$FILE $FILE
+      gsutil cp gs://broad-cda-dev/public/R3/$FILE $FILE
       #curl --remote-name https://storage.googleapis.com/broad-cda-dev/public/$FILE
     fi
 done
