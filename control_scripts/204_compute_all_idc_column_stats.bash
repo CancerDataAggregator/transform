fb5d94b7e983a59ee3a837659cb9387390883348
abradyGDIT <arthur.brady@gdit.com>
2023-06-27 14:05:43 -0400

    Updating column stats scripts at end of pipeline

---
+++ control_scripts/204_compute_all_idc_column_stats.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+chmod 755 ./package_root/auxiliary_scripts/*py
+
+echo ./package_root/auxiliary_scripts/600_count_distinct_values_by_table_and_column.IDC_CDA_data.py 
+./package_root/auxiliary_scripts/600_count_distinct_values_by_table_and_column.IDC_CDA_data.py 
+
+
---
+++ control_scripts/205_compute_all_merged_CDA_column_stats.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+chmod 755 ./package_root/auxiliary_scripts/*py
+
+echo ./package_root/auxiliary_scripts/601_count_distinct_values_by_table_and_column.all_merged_CDA_data.py
+./package_root/auxiliary_scripts/601_count_distinct_values_by_table_and_column.all_merged_CDA_data.py
+
+
