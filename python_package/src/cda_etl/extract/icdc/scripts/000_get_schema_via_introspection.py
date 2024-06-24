17d210556164300a96f4dcea2a9c76d0ad622e26
abradyGDIT <arthur.brady@gdit.com>
2024-06-24 10:46:41 -0400

    Updating Cloud SQL schema archive

--- python_package/src/cda_etl/extract/icdc/scripts/000_get_schema_via_introspection.py
+++ python_package/src/cda_etl/extract/icdc/scripts/000_get_schema_via_introspection.py
@@ -7,17 +7,13 @@ import sys
 
 from os import makedirs, path
 
-url = 'https://caninecommons.cancer.gov/v1/graphql/'
+api_url = 'https://caninecommons.cancer.gov/v1/graphql/'
 
 output_dir = path.join( 'auxiliary_metadata', '__schemas' )
 
-if not path.exists( output_dir ):
-    
-    makedirs( output_dir )
+out_json = path.join( output_dir, 'ICDC_schema.json' )
 
-out_json = f"{output_dir}/ICDC_schema.json"
-
-out_gql = f"{output_dir}/ICDC_schema.gql"
+out_gql = path.join( output_dir, 'ICDC_schema.gql' )
 
 query = '''{
   __schema {
@@ -87,29 +83,33 @@ query = '''{
   }
 }'''
 
-response = requests.post( url, json={ 'query': query } )
+if not path.exists( output_dir ):
+    
+    makedirs( output_dir )
+
+response = requests.post( api_url, json={ 'query': query } )
 
 if( response.ok ):
     
-    schema = json.loads(response.content)
+    schema = json.loads( response.content )
 
-    with open(out_json, 'w') as OUT:
+    with open( out_json, 'w' ) as OUT:
         
-        print (json.dumps(schema, indent=4, sort_keys=False), file=OUT)
+        print ( json.dumps( schema, indent=4, sort_keys=False ), file=OUT )
 
-    with open(out_gql, 'w') as OUT:
+    with open( out_gql, 'w' ) as OUT:
         
         for currentType in schema['data']['__schema']['types']:
             
-            if re.search(r'^__', currentType['name']) is None and currentType['name'] not in ['Boolean', 'Int', 'Float', 'String']:
+            if re.search( r'^__', currentType['name'] ) is None and currentType['name'] not in [ 'Boolean', 'Int', 'Float', 'String' ]:
                 
                 if currentType['kind'] == 'SCALAR':
                     
                     if currentType['description'] is not None:
                         
-                        print(f'"""{currentType["description"]}"""', file=OUT)
+                        print( f'"""{currentType["description"]}"""', file=OUT )
 
-                    print(f"scalar {currentType['name']}", file=OUT)
+                    print( f"scalar {currentType['name']}", file=OUT )
 
                 elif currentType['kind'] == 'ENUM':
                     
@@ -127,17 +127,17 @@ if( response.ok ):
 
                     if currentType['description'] is not None:
                         
-                        print(f'"""{currentType["description"]}"""', file=OUT)
+                        print( f'"""{currentType["description"]}"""', file=OUT )
 
                     # Type name.
 
                     if currentType['kind'] == 'OBJECT':
                         
-                        print(f"type {currentType['name']} {{", file=OUT)
+                        print( f"type {currentType['name']} {{", file=OUT )
 
                     else:
                         
-                        print(f"input {currentType['name']} {{", file=OUT)
+                        print( f"input {currentType['name']} {{", file=OUT )
 
                     if currentType['fields'] is None:
                         
@@ -149,17 +149,17 @@ if( response.ok ):
 
                         if field['description'] is not None:
                             
-                            print(f'  """{field["description"]}"""', file=OUT)
+                            print( f'  """{field["description"]}"""', file=OUT )
 
                         # Field name.
 
-                        print(f"  {field['name']}", end='', file=OUT)
+                        print( f"  {field['name']}", end='', file=OUT )
 
                         # Argument list.
 
-                        if field['args'] is not None and len(field['args']) > 0:
+                        if field['args'] is not None and len( field['args'] ) > 0:
                             
-                            print('(', end='', file=OUT)
+                            print( '(', end='', file=OUT )
 
                             first = True
 
@@ -167,13 +167,13 @@ if( response.ok ):
                                 
                                 if first:
                                     
-                                    print(f"{arg['name']}: ", end='', file=OUT)
+                                    print( f"{arg['name']}: ", end='', file=OUT )
 
                                     first = False
 
                                 else:
                                     
-                                    print(f", {arg['name']}: ", end='', file=OUT)
+                                    print( f", {arg['name']}: ", end='', file=OUT )
 
                                 if arg['type']['kind'] == 'NON_NULL' and arg['type']['ofType']['kind'] == 'LIST' and arg['type']['ofType']['ofType']['kind'] == 'NON_NULL' \
                                     and 'ofType' in arg['type']['ofType']['ofType'] and arg['type']['ofType']['ofType']['ofType']['kind'] in [ 'SCALAR', 'OBJECT', 'ENUM' ]:
@@ -186,11 +186,11 @@ if( response.ok ):
 
                                 elif arg['type']['kind'] == 'NON_NULL':
                                     
-                                    print(f"{arg['type']['ofType']['name']}!", end='', file=OUT)
+                                    print( f"{arg['type']['ofType']['name']}!", end='', file=OUT )
 
                                 elif arg['type']['kind'] == 'SCALAR' or arg['type']['kind'] == 'INPUT_OBJECT':
                                     
-                                    print(f"{arg['type']['name']}", end='', file=OUT)
+                                    print( f"{arg['type']['name']}", end='', file=OUT )
 
                                 elif arg['type']['kind'] == 'LIST' and arg['type']['ofType']['kind'] in [ 'SCALAR', 'OBJECT' ]:
                                     
@@ -202,23 +202,23 @@ if( response.ok ):
 
                                 else:
                                     
-                                    sys.exit( f"WTF? arg[type][name] is {arg['type']['name']}" )
+                                    sys.exit( f"Unexpected result: arg[type][name] is {arg['type']['name']}" )
 
-                            print(')', end='', file=OUT)
+                            print( ')', end='', file=OUT )
 
                         # end if ( field['args'] is not None )
 
-                        print(': ', end='', file=OUT)
+                        print( ': ', end='', file=OUT )
 
                         # Field return type.
 
-                        if field['type']['kind'] in ['SCALAR', 'OBJECT']:
+                        if field['type']['kind'] in [ 'SCALAR', 'OBJECT' ]:
                             
-                            print(field['type']['name'], file=OUT)
+                            print( field['type']['name'], file=OUT )
 
                         elif field['type']['kind'] == 'NON_NULL' and field['type']['ofType']['kind'] in [ 'SCALAR', 'OBJECT' ]:
                             
-                            print(field['type']['ofType']['name'] + '!', file=OUT)
+                            print( field['type']['ofType']['name'] + '!', file=OUT )
 
                         elif field['type']['kind'] == 'NON_NULL' and field['type']['ofType']['kind'] == 'LIST' and field['type']['ofType']['ofType']['kind'] == 'NON_NULL' and field['type']['ofType']['ofType']['ofType']['kind'] in [ 'SCALAR', 'OBJECT', 'ENUM' ]:
                             
@@ -234,27 +234,27 @@ if( response.ok ):
 
                             if field['type']['ofType']['name'] is None:
                                 
-                                print(f"[[{field['type']['ofType']['ofType']['name']}]]", file=OUT)
+                                print( f"[[{field['type']['ofType']['ofType']['name']}]]", file=OUT )
 
                             else:
                                 
-                                print(f"[{field['type']['ofType']['name']}]", file=OUT)
+                                print( f"[{field['type']['ofType']['name']}]", file=OUT )
 
                         else:
                             
-                            sys.exit( f"WTF? field[type][kind] is {field['type']['kind']}" )
+                            sys.exit( f"Unexpected result: field[type][kind] is {field['type']['kind']}" )
 
                     print('}', file=OUT)
 
                 else:
                     
-                    sys.exit( f"WTF? currentType['kind'] is {currentType['kind']}" )
+                    sys.exit( f"Unexpected result: currentType['kind'] is {currentType['kind']}" )
 
-                print(file=OUT)
+                print( file=OUT )
 
 else:
     
-    print(query)
+    print( query )
 
     # If response code is not ok (200), print the resulting http error code with description
 
