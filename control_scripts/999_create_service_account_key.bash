919d662ec36f36bee960e58745f36f0d8700faa0
abradyGDIT <arthur.brady@gdit.com>
2024-08-29 13:19:49 -0400

    Updating control script structures

--- control_scripts/999_create_service_account_key.bash
+++ control_scripts/999_create_service_account_key.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-KEY_FILE=GCS-service-account-key.etl-github-testing.json
+KEY_FILE=GCS-service-account-key.json
 
 SA_NAME=YOUR_SERVICE_ACCOUNT_NAME
 
@@ -9,4 +9,5 @@ PROJECT_ID=YOUR_PROJECT_ID
 gcloud config set project $PROJECT_ID
 
 gcloud iam service-accounts keys create $KEY_FILE --iam-account=${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
+
 
